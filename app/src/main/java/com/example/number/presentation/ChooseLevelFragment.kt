ec8cee25a412313fc990369488e741ac71b120b7
Anna <ap554156@yandex.ru>
2023-07-02 21:10:35 +0300

    update

---
+++ app/src/main/java/com/example/number/presentation/ChooseLevelFragment.kt
@@ -0,0 +1,33 @@
+package com.example.number.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.number.databinding.FragmentChooseLevelBinding
+
+class ChooseLevelFragment : Fragment() {
+
+    private var _binding: FragmentChooseLevelBinding? = null
+    private val binding: FragmentChooseLevelBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentChooseLevelBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
